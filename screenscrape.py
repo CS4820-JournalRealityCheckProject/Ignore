876f8fcd4ac91493f2c887ebce5cc5e91730057c
matt bujold <matt474@live.ca>
2019-01-28 13:36:40 -0400

    Created method to scrape  Springer site

--- screenscrape.py
+++ screenscrape.py
@@ -1,10 +1,11 @@
 import requests
 import xml.etree.ElementTree as ET
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 doiList = ['10.1016/j.ijrmhm.2018.07.009', '10.1016/j.burnso.2018.03.001']
 #[0] Science direct
 #[1] Science direct(Open Access)
+springerDoi = '10.1007/s10059-013-0080-3'
 
 def ScienceDirect(doi, key):
     parameters ={"APIKey" : key}
@@ -18,8 +19,20 @@ def ScienceDirect(doi, key):
             return True
     return False
 
+def Springer(doi):
+    url = 'https://link.springer.com/article/'+doi
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for header in soup.find_all('div', {"id": "article_no_access_banner"}):
+        return False
+    return True
+
 key = open("ApiKeys/ScienceDirect.txt").read()
 #print(key)
 for doi in doiList:
     result = ScienceDirect(doi, key)
     print ("("+str(result)+") "+doi)
+
+result = Springer(springerDoi)
+print ("("+str(result)+") "+springerDoi)
