fdc8203f04d42668f72c84662f6189f60ee5cd29
matt bujold <matt474@live.ca>
2019-01-29 10:11:36 -0400

    Added method to check oxford journals, will need more testing

--- screenscrape.py
+++ screenscrape.py
@@ -2,11 +2,6 @@ import requests
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-doiList = ['10.1016/j.ijrmhm.2018.07.009', '10.1016/j.burnso.2018.03.001']
-#[0] Science direct
-#[1] Science direct(Open Access)
-springerDoi = '10.1007/s10059-013-0080-3'
-
 def ScienceDirect(doi, key):
     parameters ={"APIKey" : key}
     r = requests.get("https://api.elsevier.com/content/article/doi/"+doi, params = parameters)
@@ -24,9 +19,36 @@ def Springer(doi):
 
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    for header in soup.find_all('div', {"id": "article_no_access_banner"}):
+    if soup.find('div', {"id": "article_no_access_banner"}):
         return False
     return True
+
+def Oxford(doi):
+    journal = doi.split('/')[1]
+
+    url = 'https://academic.oup.com/'+journal+'/article-lookup/doi/'+doi
+
+    #Lie about who we are to get access
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    if soup.find('div', {"class": "article-top-info-user-restricted-options"}):
+        return False
+    return True
+
+
+#Code down here is messy,
+#was just quickly thrown together to test the above
+
+doiList = ['10.1016/j.ijrmhm.2018.07.009', '10.1016/j.burnso.2018.03.001']
+#[0] Science direct
+#[1] Science direct(Open Access)
+springerDoi = '10.1007/s10059-013-0080-3'
+
+print(Oxford('10.1093/jigpal/jzy015'))
 
 key = open("ApiKeys/ScienceDirect.txt").read()
 #print(key)
