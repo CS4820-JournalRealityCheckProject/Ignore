d874e9782362eb30dd5c95f8b04be42914702d67
matt bujold <matt474@live.ca>
2019-01-30 13:48:11 -0400

    slight formatting changes

--- screenscrape.py
+++ screenscrape.py
@@ -2,20 +2,20 @@ import requests
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-articleList = [['Oxford Journal', '10.1093/jigpal/jzy015'],
-               ['Oxford Journal', '10.1111/j.1095-8339.2011.01155.x'], #Does not work with code
-               ['Oxford Journal', '10.1111/bij.12521'],
-               ['Science Direct', '10.1016/j.ijrmhm.2018.07.009'],
-               ['Science Direct', '10.1016/j.burnso.2018.03.001'], #Open access
-               ['Springer', '10.1007/s10059-013-0080-3'],
-                  ]
+article_list = [['Oxford Journal', '10.1093/jigpal/jzy015'],
+                ['Oxford Journal', '10.1111/j.1095-8339.2011.01155.x'],
+                ['Oxford Journal', '10.1111/bij.12521'],
+                ['Science Direct', '10.1016/j.ijrmhm.2018.07.009'],
+                ['Science Direct', '10.1016/j.burnso.2018.03.001'],  #Open access
+                ['Springer', '10.1007/s10059-013-0080-3'],
+               ]
 
-def DOItoURL(doi):
+def doi_to_url(doi):
     url = "http://dx.doi.org/" + doi
     r = requests.get(url, allow_redirects=False)
     return (r.headers['Location'])
 
-def ScienceDirect(doi, key):
+def science_direct(doi, key):
     parameters ={"APIKey" : key}
     r = requests.get("https://api.elsevier.com/content/article/doi/"+doi, params = parameters)
 
@@ -26,7 +26,7 @@ def ScienceDirect(doi, key):
             return True
     return False
 
-def Springer(doi):
+def springer(doi):
     url = 'https://link.springer.com/article/'+doi
 
     r = requests.get(url)
@@ -35,8 +35,8 @@ def Springer(doi):
         return False
     return True
 
-def Oxford(doi):
-    url = DOItoURL(doi)
+def oxford(doi):
+    url = doi_to_url(doi)
 
     #Lie about who we are to get access
     headers = {
@@ -55,11 +55,11 @@ def Oxford(doi):
 
 keySD = open("ApiKeys/ScienceDirect.txt").read()
 
-for article in articleList:
+for article in article_list:
     if article[0] == 'Oxford Journal':
-        result = Oxford(article[1])
+        result = oxford(article[1])
     elif article[0] == 'Springer':
-            result = Springer(article[1])
+            result = springer(article[1])
     elif article[0] == 'Science Direct':
-            result = ScienceDirect(article[1], keySD)
+            result = science_direct(article[1], keySD)
     print(str(result)+": "+article[1])
